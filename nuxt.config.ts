ed279615f7d7a960c56cb084caa1fa76011b93b3
Rtaaaaabo <r.taaaaabo@gmail.com>
2024-03-17 01:03:33 +0900

    Nux pagetとComponentの作成

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  vite: {
+    vue: {
+      customElement: true,
+    },
+    vueJsx: {
+      mergeProps: true,
+    },
+  },
+  devtools: { enabled: true },
+  $production: {
+    routeRules: {
+      "/**": { isr: true },
+    },
+  },
+  $development: {
+    //
+  },
+});
